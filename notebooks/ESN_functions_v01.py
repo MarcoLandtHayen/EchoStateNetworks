b602b8afbe84022c5f8c0c7c4c5c0a03ee76ba33
MarcoLandtHayen <malaha@gmx.net>
2022-02-10 17:25:46 +0100

    finished working on ESN_exp02, extract results in ppt

--- notebooks/ESN_functions_v01.py
+++ notebooks/ESN_functions_v01.py
@@ -22,6 +22,9 @@ import matplotlib.pyplot as plt
 # time_lag (int): Necessary when creating inputs for multi-reservoir ESN, having timelag 0,1,2,... days between
 #                 input and target.
 # train_val_split ([0,1]): rel. amount of input samples used for training
+# val_samples_from ('end', 'start_end'): Determines where to take validation samples from. Usually take
+#                                        samples from series 'end' as default, but can also take equal
+#                                        number of samples from beginning AND end ('start_end').
 # abs_to_rel_YN (True/False): if True, convert absolute values to relative change values
 # scaled_YN (True/False):if True, scale inputs and targets to [0,1] applying min/max scaling 
 #                        according to min/max obtained from only train inputs
@@ -33,7 +36,7 @@ import matplotlib.pyplot as plt
 # Returns train_min and train_max, used for scaling, set to ZERO, if scaled_YN=False.
 
 def split_data(data_abs, input_length, target_length, time_lag=0, 
-               train_val_split=0.8, abs_to_rel_YN=True, binary_YN=False, scaled_YN=False,
+               train_val_split=0.8, val_samples_from='end', abs_to_rel_YN=True, binary_YN=False, scaled_YN=False,
                verbose=True):
     
     # Optionally convert input data from absolute values to rel. change values.
@@ -85,14 +88,26 @@ def split_data(data_abs, input_length, target_length, time_lag=0,
     # re-assign obtained X_out to X
     X = X_out    
     
+    # Split inputs and targets into train and validation data according to train_val_split and
+    # desired val_samples_from attribute.
     
-    # Split inputs and targets into train and validation data according to train_val_split
-    n_train = int(len(X) * train_val_split)
+    # If validation samples are supposed to be from 'end' of series:
+    if val_samples_from == 'end':
+        n_train = int(len(X) * train_val_split)
     
-    train_input = X[:n_train]
-    val_input = X[n_train:]
-    train_target = Y[:n_train]
-    val_target = Y[n_train:]
+        train_input = X[:n_train]
+        val_input = X[n_train:]
+        train_target = Y[:n_train]
+        val_target = Y[n_train:]
+    
+    # If validation samples are supposed to be from 'beginning' and 'end':
+    else:
+        n_train = int(len(X) * (1-train_val_split)/2)
+        
+        train_input = X[n_train:-n_train]
+        train_target = Y[n_train:-n_train]
+        val_input = np.concatenate((X[:n_train], X[-n_train:]))
+        val_target = np.concatenate((Y[:n_train], Y[-n_train:]))
   
     # Optionally scale train and validation inputs and targets to [0,1] 
     # according to min/max obtained from train inputs.
@@ -349,6 +364,9 @@ def predESN(W_in, W_res, W_out, val_input, val_target, activation='tanh', abs_va
 ## Parameters:
 # model: trained CNN/LSTM model
 # target_length (int): Necessary information to reconstruct absolute values from relativ change values.
+# abs_values_YN (True/False): Flags if we have absolute values as function input. This is important for
+#                             correctly calculating accuracy. Function output series are absolute values
+#                             in any case.
 # scaled_YN (True/False): Flags if we have scaled inputs and targets. This is important to know, since  
 #                         calculating the accuracy requires un-scaled rel. change values.
 # train_min / train_max: If we have scaled inputs, we need min and max values used for scaling.
@@ -356,17 +374,20 @@ def predESN(W_in, W_res, W_out, val_input, val_target, activation='tanh', abs_va
 # verbose (True/False)): if True, print shapes of function input and output plus evaluation metrics
 
 ## Function output:
-# Returns array containing true targets val_target (num samples), as un-scaled absolute values.
-# Returns array containing predictions val_pred (num samples), as un-scaled absolute values.
+# Returns array containing true targets val_target (num samples), as un-scaled absolute or
+# rel. change values, depending on attribute abs_values_YN: If True return absolute values, else rel. changes.
+# Returns array containing predictions val_pred (num samples), as un-scaled absolute or
+# rel. change values, depending on attribute abs_values_YN: If True return absolute values, else rel. changes.
 # Returns accuracy as amount of correctly predicted up/down movements.
 # Returns mean-absolute-error mae_rel_chg and mae_abs for deviation of predicted values from true targets,
 # as rel. change values and absolute values, respectively.
 
-def predCNNLSTM(val_input, val_target, model, target_length=1, scaled_YN=False, train_min=0.0, train_max=0.0, abs_base=1.0, verbose=True):
+def predCNNLSTM(val_input, val_target, model, target_length=1, abs_values_YN=False,
+                scaled_YN=False, train_min=0.0, train_max=0.0, abs_base=1.0, verbose=True):
                        
     # Get predicted output from trained model for all validation inputs.
-    # Note: val_pred comes with shape (1, num samples), need to get rid of first dimension.
-    val_pred = model.predict(val_input)
+    # Note: val_pred comes with shape (num samples, 1), need to get rid of second dimension.
+    val_pred = model.predict(val_input)[:,0]
     
     # Optionally re-scale target and prediction before calculating accuracy on un-scaled rel. change values.
     if scaled_YN:
@@ -376,50 +397,41 @@ def predCNNLSTM(val_input, val_target, model, target_length=1, scaled_YN=False, 
              
     ## Evaluate ESN prediction
     
-    # Function input is given as rel. change values and is now un-scaled, 
-    # as required for calculating balance and accuracy.
+    # Function input was given as rel. change or absolute values and is now un-scaled.    
+    # Temporarily create rel. change target and prediction, if absolute values are given as function input,
+    # because calculating balance and accuracy requires un-scaled rel. change values.
     
-    # check balance in up/down movements of validation data
-    val_balance = np.round(sum(val_target>=0) / len(val_target), 3)
-
-    # get prediction accuracy from rel. change prediction
-    accuracy = np.round(sum(np.sign(val_target) == np.sign(val_pred[:,0])) / len(val_target), 3)
-
-    # Get mean-absolute-error for deviation of predicted values from true targets, using rel. change values!
-    mae_rel_chg = np.round(sum(np.abs(val_target - val_pred[:,0])) / len(val_target), 4)
-
-    ## Now re-convert rel. change values to absolute values:   
+    if abs_values_YN:
+                       
+        # Create rel. change targets
+        val_target_temp = (val_target[1:] - val_target[:-1]) / val_target[:-1]
+        val_pred_temp = (val_pred[1:] - val_pred[:-1]) / val_pred[:-1]  
     
-    # Initialize storage for time series with absolute values.
-    val_target_abs = np.zeros(len(val_target))
-    val_pred_abs = np.zeros(len(val_target))
-    
-    # Loop over input series, optionally hook on base value
-    for i in range(len(val_target)):
+        # check balance in up/down movements of validation data
+        val_balance = np.round(sum(val_target_temp>=0) / len(val_target_temp), 3)
         
-        # First target_length values are hooked on the same base value (or default: 1), for simplicity.
-        # To be more accurate, one needed target_length base values to hook on.
-        if i < target_length:
-            val_target_abs[i] = abs_base * (1 + val_target[i])
-            val_pred_abs[i] = abs_base * (1 + val_pred[i,0])
-        # Here: One-step prediction with "teacher-forcing", hence hook on true absolute values.
-        # Note: Tage target_length into account, to hook on correct true value: target_length steps back.
-        elif i >= target_length:
-            val_target_abs[i] = val_target_abs[i-target_length] * (1 + val_target[i])
-            val_pred_abs[i] = val_target_abs[i-target_length] * (1 + val_pred[i,0])
+        # get prediction accuracy from rel. change prediction
+        accuracy = np.round(sum(np.sign(val_target_temp) == np.sign(val_pred_temp)) / len(val_target_temp), 3)
 
-    # Get mean-absolute-error for deviation of predicted values from true targets, using absolute values!
-    mae_abs = np.round(sum(np.abs(val_target_abs - val_pred_abs)) / len(val_target_abs), 4)
+    # If function input is already given as rel. change values, can directly calculate balance and accuracy
+    else:
+        # check balance in up/down movements of validation data
+        val_balance = np.round(sum(val_target>=0) / len(val_target), 3)
+        
+        # get prediction accuracy from rel. change prediction
+        accuracy = np.round(sum(np.sign(val_target) == np.sign(val_pred)) / len(val_target), 3)
 
-
+    # get mean-absolute-error for deviation of predicted values from true targets
+    mae = np.round(sum(np.abs(val_target - val_pred)) / len(val_target), 4)
+         
     
     # Optionally print dimensions and plot true vs. predicted absolute values
     if verbose:
         
-        # Fidelity check: Plot true vs. predicted absolute values
+        # Fidelity check: Plot true vs. predicted target values
         plt.figure(figsize=(16,8))
-        plt.plot(range(len(val_target_abs)),val_target_abs,'b',label="true data", alpha=0.3)
-        plt.plot(range(len(val_pred_abs)),val_pred_abs,'k',  alpha=0.8, label='pred data')
+        plt.plot(range(len(val_target)),val_target,'b',label="true data", alpha=0.3)
+        plt.plot(range(len(val_pred)),val_pred,'k',  alpha=0.8, label='pred data')
         plt.legend()
         plt.show()
         
@@ -429,16 +441,13 @@ def predCNNLSTM(val_input, val_target, model, target_length=1, scaled_YN=False, 
         print("val_input shape: ", val_input.shape)
         print("val_target shape: ", val_target.shape)
         print("val_pred shape: ", val_pred.shape)     
-        print("val_target_abs shape: ", val_target_abs.shape)
-        print("val_pred_abs shape: ", val_pred_abs.shape)     
         print("up movements percentage in val_target: ", val_balance)
         print("ESN pred. accuracy: ", accuracy)
-        print("ESN mean abs. error (rel. change values): ", mae_rel_chg)
-        print("ESN mean abs. error (absolute values): ", mae_abs)
-
+        print("ESN mean abs. error: ", mae)
+        
     
     # return values
-    return val_target_abs, val_pred_abs, accuracy, mae_rel_chg, mae_abs
+    return val_target, val_pred, accuracy, mae
 
 
 
