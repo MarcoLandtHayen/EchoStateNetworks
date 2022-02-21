0a0efc92b8d459c38fa2e98c42f352ff0d4fd7fd
MarcoLandtHayen <malaha@gmx.net>
2022-02-21 17:06:00 +0100

    Started to work on ESN_exp04: Set up function for decomposing and splitting input series

--- notebooks/ESN_functions_v02.py
+++ notebooks/ESN_functions_v02.py
@@ -17,6 +17,39 @@ from tensorflow.keras.utils import plot_model
 
 
 
+### Dummy model für Austausch mit Willi
+
+temp_init = tf.constant_initializer(value=np.zeros(100))
+temp_init = tf.constant_initializer(value=np.linspace(1,100,100))
+
+
+# Input layer
+model_inputs = Input(shape=(10,1)) # (timesteps, features)
+print(model_inputs)
+
+# Dense output layer on top
+#output = Dense(units=100, kernel_initializer=tfi.RandomUniform(minval=-1, maxval=1, seed=None))(model_inputs)
+output = Dense(units=100, kernel_initializer=temp_init)(model_inputs)
+print(output)
+
+# Define model
+model = Model(model_inputs, output)
+model.summary()
+
+temp_weights = model.get_weights()[0]
+temp_weights.shape
+
+# check distribution of input values after transformation
+nBins = 100
+fig, axes = plt.subplots(1, 1, figsize=(10,5))
+axes.hist(temp_weights[0], nBins, color="red")
+plt.show()
+
+#################################
+
+
+
+
 ### Set up custom layer
 class CustomLayer(tf.keras.layers.Layer):
     def __init__(self, n_res, W_in_lim=1, leak_rate=0.5, verbose=0):
@@ -38,15 +71,11 @@ class CustomLayer(tf.keras.layers.Layer):
                                                bias_initializer=tfi.RandomUniform(minval=-W_in_lim, maxval=W_in_lim, seed=None))
         self.verbose= verbose
     
-    
-    
-    
     def call(self, inputs):
     
         # Connect inputs to reservoir units to get initial reservoir state (t=1), called x_prev, since
         # it will be used as "previous" state when calculating further reservoir states.
         x_prev = self.leak_rate * tf.tanh(self.res_units_init(inputs[:,0:1,:])) # x(1) = leak_rate * tanh(W_in * u(1))
-        print("\ninitial x_prev: ", x_prev)
         
         # Initialize storage X for all reservoir states (samples, timesteps, n_res):
         # Store x_prev as x_1 in X
@@ -54,7 +83,6 @@ class CustomLayer(tf.keras.layers.Layer):
         
         # Now loop over remaining time steps t = 2..T
         T = inputs.shape[1]
-        print("\nT: ", T)
         
         for t in range(1,T):
             x_t = (1 - self.leak_rate) * x_prev + self.leak_rate * tf.tanh(self.res_units_init(inputs[:,t:t+1,:]) + self.res_units(x_prev))
@@ -65,74 +93,44 @@ class CustomLayer(tf.keras.layers.Layer):
             # x_t becomes x_prev for next timestep
             x_prev = x_t
         
-        print("\nX shape: ", X.shape)
         return X
         
-            
-        
-        
-    
-    ## Use X to store all n_res final reservoir states after T input steps
-    ## for all n_samples train inputs, use specified activation function.
-    
-    # initialize X (n_res x n_samples)
-    X = np.zeros((n_res,n_samples))
-    
-    # Loop over n_samples    
-    for i in range(n_samples):
-        
-        # Loop over timesteps in current sample
-        for j in range(T):
-            
-            # If desired activation function is 'tanh':
-            if activation=='tanh':
-                # first input timestep needs special treatment, since reservoir state is not yet initialized
-                if j == 0:
-                    X[:,i:i+1] = np.tanh(W_in * train_input[i,j])
-                elif j > 0:
-                    X[:,i:i+1] = np.tanh(W_in * train_input[i,j] + np.reshape(np.matmul(W_res, X[:,i:i+1]),(n_res,1)))
-            # If desired activation function is 'sigmoid'
-            elif activation=='sigmoid':
-                # first input timestep needs special treatment, since reservoir state is not yet initialized
-                if j == 0:
-                    X[:,i:i+1] = expit(W_in * train_input[i,j])
-                elif j > 0:
-                    X[:,i:i+1] = expit(W_in * train_input[i,j] + np.reshape(np.matmul(W_res, X[:,i:i+1]),(n_res,1)))
-            
-            
-            
-            
-
+             
 
 ### Set up model WITH custom layer
+## Here: Modification of reservoir weights (according to desired sparsity and spec_rad) AFTER
+## model declaration.
+## !!! Refactor: Set up custom initializer and use it INSIDE class definition (tf.constant_initializer?).
 
 # Set (hyper-)parameters:
-n_res = 20 # Number of reservoir units
+n_res = 5 # Number of reservoir units
 W_in_lim = 1 # Initialize input weights from random uniform distribution in [- W_in_lim, + W_in_lim]
 leak_rate = 0.5 # Leak rate for transition of reservoir states
 spec_radius = 1.2 # Spectral radius, becomes largest Eigenvalue of reservoir weight matrix
 sparsity = 0.2 # Sparsity of reservoir weight matrix
+input_length = 2 # Input length
+n_features = 1 # Number of input features
 
 # Input layer
-model_inputs = Input(shape=(10,2)) # (timesteps, features)
+model_inputs = Input(shape=(input_length,n_features)) # (timesteps, features)
 print(model_inputs)
 
 # Use custom layer
-custom_layer = CustomLayer(n_res=n_res, W_in_lim=W_in_lim, leak_rate=leak_rate)(model_inputs) # 20 reservoir units
+custom_layer = CustomLayer(n_res=n_res, W_in_lim=W_in_lim, leak_rate=leak_rate)(model_inputs) # n_res reservoir units
 print(custom_layer)
-
 
 # Output unit
 output = Dense(1, name='output')(custom_layer)
 print(output)
 
 # Define model
-model = Model(model_inputs, output, name='model_2')
+model = Model(model_inputs, output, name='model')
 model.summary()
 plot_model(model, show_shapes=True, show_layer_names=True)
 
 
 ## Modify reservoir weights W_res using spectral radius:
+# !!! Need to refactor code: Use customized kernel_initializer instead, e.g. constant_initializer()
 
 # Get model weights for ALL layers
 model_weights = np.array(model.get_weights())
@@ -141,9 +139,11 @@ print("\model_weights shape: ", model_weights.shape)
 # Extract reservoir weights
 W_res = model_weights[2]
 print("\nW_res: ", W_res)
+print("W_res shape: ", W_res.shape)
 
-# Check shape, expect (n_res, n_res)
-print("\nW_res shape: ", W_res.shape)
+# Check sparsity and max. Eigenvalue
+print("\nW_res sparsity: ", sum(sum(W_res != 0)) / (W_res.shape[0]**2))
+print("W_res max EV: ", np.max(np.real((np.linalg.eigvals(W_res)))))
 
 # Need temporary matrix W_temp to implement sparsity manually
 W_temp = np.random.uniform(low=0, high=1, size=(n_res,n_res))
@@ -161,6 +161,86 @@ W_res = spec_radius * W / ev_max
 # Integrate modified reservoir weights into model weights
 model_weights[2] = W_res
 model.set_weights(model_weights)
+
+
+## Try model:
+
+# Create input samples:
+n_samples = 1
+temp_input = np.ones((n_samples,input_length,n_features))
+print("\nshape of model input: ", temp_input.shape)
+
+# Get model prediction
+temp_pred = model.predict(temp_input)
+print("\nshape of model output: ", temp_pred.shape)
+
+# Get model weights for ALL layers
+model_weights = np.array(model.get_weights())
+print("\nmodel_weights shape: ", model_weights.shape)
+
+## Extract weights and biases
+
+# Input to Reservoir
+W_in = model_weights[0]
+b_in = model_weights[1]
+print("\nW_in: (shape: ", W_in.shape, "\n", W_in)
+print("\nb_in: (shape: ", b_in.shape, "\n", b_in)
+
+# Reservoir recurrent
+W_res = model_weights[2]
+b_res = model_weights[3]
+print("\nW_res: (shape: ", W_res.shape, "\n", W_res)
+print("\nb_res: (shape: ", b_res.shape, "\n", b_res)
+
+# Reservoir to Output
+W_out = model_weights[4]
+b_out = model_weights[5]
+print("\nW_out: (shape: ", W_out.shape, "\n", W_out)
+print("\nb_out: (shape: ", b_out.shape, "\n", b_out)
+
+# Extract first input sample, first and second timestep, need shape (timestep, feature)
+input_1_1 = temp_input[0, 0:1, 0:1]
+input_1_2 = temp_input[0, 1:2, 0:1]
+
+print("\nFirst input, first timestep, shape: ", input_1_1.shape)
+
+# Get initial reservoir activation for first input sample, first timestep:
+W_res_act_1_1 = leak_rate * np.tanh(np.matmul(input_1_1, W_in) + b_in)
+W_res_act_1_1
+
+# Get reservoir state after recursion (second timestep):
+W_res_act_1_2 = (1 - leak_rate) * W_res_act_1_1 + leak_rate * np.tanh(np.matmul(input_1_2, W_in) + b_in + np.matmul(W_res_act_1_1, W_res) + b_res)
+W_res_act_1_2
+
+# Get output activation for first input sample after second timestep:
+W_out_act_1_2 = np.matmul(W_res_act_1_2, W_out) + b_out
+W_out_act_1_2
+
+# Compare to model output for first input sample after second timestep:
+temp_pred[0, 1:2, :]
+
+
+## Try "short" model, including only Input and Reservoir:
+    
+# Define "short" model
+model_in_res = Model(model_inputs, custom_layer, name='model_in_res')
+model_in_res.summary()
+
+# Get model prediction
+model_in_res_pred = model_in_res.predict(temp_input)
+print("\nshape of model output: ", model_in_res_pred.shape)
+
+# Compare initial reservoir activation from "short" model to manually calculated:
+model_in_res_pred[0,0:1,:]
+W_res_act_1_1
+
+# Compare reservoir state after recursion (second timestep) from "short" model to manually calculated:
+model_in_res_pred[0,1:2,:]
+W_res_act_1_2
+
+
+
+
 
 
 
